076bfefa0e1d70ab6be0b862c838fd8662b7b57e
Chemutai Kevin <kchemutai@gmail.com>
2024-05-28 23:26:24 -0500

    Initial working code Lab1B

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1021L, "Daniel", "Agar", LocalDate.of(2018,1,17),945.50, new PensionPlan("EX1089", LocalDate.of(2023, 1, 17), 100.00)));
+        employees.add(new Employee(1022L, "Benard", "Show", LocalDate.of(2019,5,3),197750.00, null));
+        employees.add(new Employee(1023L, "Carly", "Agar", LocalDate.of(2014,5,16),842000.75, new PensionPlan("SM2307", LocalDate.of(2019,11,4), 1555.50)));
+        employees.add(new Employee(1024L, "Wesley", "Schneider", LocalDate.of(2019,5,2),74500.00, null));
+
+        List<Employee> monthlyUpcomingEnrollees= getMonthlyUpcomingEnrollees(employees);
+
+        //sort the employees
+        List<Employee> sortedEmployees = monthlyUpcomingEnrollees.stream().sorted(Comparator.comparing(Employee::getLastName))
+                .sorted(Comparator.comparing((Employee::getYearlySalary)).reversed())
+                .collect(Collectors.toList());
+        printEmployeesInJson(sortedEmployees);
+    }
+
+    private static List<Employee> getMonthlyUpcomingEnrollees(List<Employee> employees) {
+        LocalDate now = LocalDate.now();
+        LocalDate nextMonthStart = now.plusMonths(1).withDayOfMonth(1);
+        LocalDate nextMonthEnd = nextMonthStart.withDayOfMonth(nextMonthStart.lengthOfMonth());
+
+        return employees.stream()
+                .filter(e -> e.getPensionPlan() == null)
+                .filter(e -> ChronoUnit.YEARS.between(e.getEmploymentDate(), nextMonthEnd) >= 5)
+                .collect(Collectors.toList());
+    }
+
+    private static  void printEmployeesInJson(List<Employee> employees){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        try {
+            // Convert list of employees to JSON string
+            String jsonString = objectMapper.writeValueAsString(employees);
+            // Print the JSON string
+            System.out.println(jsonString);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/PensionPlan.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class PensionPlan{
+    private String planReferenceNumber;
+    private LocalDate enrollmentDate;
+    private Double monthlyContribution;
+
+    public PensionPlan() {}
+
+    public PensionPlan(String planReferenceNumber, LocalDate enrollmentDate, Double monthlyContribution) {
+        this.planReferenceNumber = planReferenceNumber;
+        this.enrollmentDate = enrollmentDate;
+        this.monthlyContribution = monthlyContribution;
+    }
+
+    public String getPlanReferenceNumber() {
+        return planReferenceNumber;
+    }
+
+    public void setPlanReferenceNumber(String planReferenceNumber) {
+        this.planReferenceNumber = planReferenceNumber;
+    }
+
+    public LocalDate getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(LocalDate enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public Double getMonthlyContribution() {
+        return monthlyContribution;
+    }
+
+    public void setMonthlyContribution(Double monthlyContribution) {
+        this.monthlyContribution = monthlyContribution;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("PensionPlan{");
+        sb.append("planReferenceNumber='").append(planReferenceNumber).append('\'');
+        sb.append(", enrollmentDate=").append(enrollmentDate);
+        sb.append(", monthlyContribution=").append(monthlyContribution);
+        sb.append('}');
+        return sb.toString();
+    }
+}
